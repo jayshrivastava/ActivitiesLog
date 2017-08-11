94f5c99cabe4416433abd869267b1d1c2b5417d9
unknown <jayshrivastava15@gmail.com>
2017-08-10 20:50:27 -0400

    added an html form which posts to the database. s/o samuel yang

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 var express = require('express');
 var sqlite3 = require('sqlite3');
+var bodyParser = require('body-parser')
 var path = require('path');
-// var fetch = require ('node-fetch');
 var db = new sqlite3.Database('NodeBlog.db');
 var app = express();
 var port = 3000;
 
-// var getInit = { method: 'GET'};
-// var postInit = {method: 'POST'};
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// fetch('https://localhost:2000/Entries', getInit)
-//     .then(function(data) {
-//       console.log("Sent Entries");
-//     })
-//     .catch(function(error) {
-//     console.log("Error");
-//     });
+// parse application/json
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -30,11 +25,7 @@ app.get('/Entries', function(request, response){
 });
 // FIX DB.RUN
 app.post('/postEntry', function(request, response) {
-  var t= request.Title;
-  var m =request.Month;
-  var y = request.Year;
-  var p = request.Paragraph;
-    db.run("INSERT INTO Entries (Title, Month, Year, Body) VALUES (?, ?, ?,?)",t,m,y,p,
+    db.run("INSERT INTO Entries (Title, Month, Year, Body) VALUES (?,?,?,?)", request.body.Title,request.body.Month, request.body.Year, request.body.Paragraph ,
     function (err, rows) {  console.log("Express POST recieved on port: " + port);
     response.send(rows);
   });
